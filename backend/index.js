6f5c05e3533e1a4f0af366445085ba32b642a408
Saurabh <nucleusbentok@gmail.com>
2025-09-01 00:31:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://onecart-583n.onrender.com", "http://localhost:5174"],
+    origin: ["https://onecart-583n.onrender.com", "https://onecart-admin-3hxe.onrender.com"],
     credentials: true,
   })
 );
