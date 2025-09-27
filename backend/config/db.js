084195fa241489dd85a77226fec993d89d86c989
Saurabh <nucleusbentok@gmail.com>
2025-09-28 00:32:43 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONOGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected");
   } catch (error) {
     console.log(`Database error :=  ${error}`);
